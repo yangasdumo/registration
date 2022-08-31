5d46382c2bb0bd0e7b9d9780c96f772a41f7db92
Yanga <yangasiqiki@gmail.com>
2022-08-31 09:42:42 +0200

    changes

---
+++ test/registration-test.js
@@ -0,0 +1,24 @@
+// const assert = require('assert');
+// const Greet = require('');
+// const pgPromise = require('pg-promise');
+// const greetings = require('');
+
+// const pgp = pgPromise({})
+
+// const local_database_url = 'postgres://codex:codex123@localhost:5432/my_regnumber';
+// const connectionString = local_database_url;
+
+
+// const db = pgp(connectionString);
+
+//  const greeted = greetings(db)
+
+// // describe("The Greeting Database tests", async function () {
+
+// //     beforeEach(async function(){
+// //         await db.none('delete from my_greet where id >=1')
+
+
+// after(async function () {
+//     await db.manyOrNone('Truncate my_greet');
+// });
